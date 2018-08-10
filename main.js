a3712f8e0c034b2f98318ff424db4a150e9155d3
Marc Juul <github@juul.io>
2018-08-10 01:10:43 -0700

    added code for showing points

--- main.js
+++ main.js
@@ -4,10 +4,45 @@ import MVT from 'ol/format/MVT.js';
 
 import VectorTileLayer from 'ol/layer/VectorTile.js';
 import VectorTileSource from 'ol/source/VectorTile.js';
+import {Vector as VectorLayer} from 'ol/layer.js';
+
+import Feature from 'ol/Feature.js';
+import VectorSource from 'ol/source/Vector.js';
+import Point from 'ol/geom/Point.js';
+
 import {Fill, Icon, Stroke, Style, Text} from 'ol/style.js';
 
 import customStyle from './custom_style.js';
 
+// EPSG:3857
+var sudoroomLonLat = [-122.2663397, 37.8350257];
+var sudoroomCoords = fromLonLat(sudoroomLonLat, 'EPSG:3857');
+
+var pointFeature = new Feature({
+  geometry: new Point(sudoroomCoords),
+  name: 'Null Island',
+  population: 4000,
+  rainfall: 500
+});
+
+var pointStyle = new Style({
+  image: new Icon({
+    anchor: [0.5, 46],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+    src: 'icon.png'
+  })
+});
+
+pointFeature.setStyle(pointStyle);
+
+var pointSource = new VectorSource({
+  features: [pointFeature]
+});
+
+var pointLayer = new VectorLayer({
+  source: pointSource
+});
 
 var style = new Style({
   fill: new Fill({
@@ -45,17 +80,15 @@ var mainLayer = new VectorTileLayer({
 
 })
 
-// EPSG:3857
-var sudoroomLonLat = [-122.2663397, 37.8350257];
-
 var myMap = new Map({
   target: 'map',
   layers: [
-    mainLayer
+    mainLayer,
+    pointLayer
   ],
 
   view: new View({
-    center: fromLonLat(sudoroomLonLat, 'EPSG:3857'),
+    center: sudoroomCoords,
     zoom: 10
   })
 
