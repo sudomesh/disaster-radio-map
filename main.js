549d084c7e31ea1e1476c9bf8cf9b717fc8ee406
Marc Juul <github@juul.io>
2018-08-11 17:32:13 -0700

    added pin-dropping and popups when clicking pins

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ import VectorTileLayer from 'ol/layer/VectorTile.js';
 import VectorTileSource from 'ol/source/VectorTile.js';
 import {Vector as VectorLayer} from 'ol/layer.js';
 
+import Overlay from 'ol/Overlay.js';
+
 import Feature from 'ol/Feature.js';
 import VectorSource from 'ol/source/Vector.js';
 import Point from 'ol/geom/Point.js';
@@ -16,6 +18,7 @@ import customStyle from './custom_style.js';
 
 // EPSG:3857
 var sudoroomLonLat = [-122.2663397, 37.8350257];
+var fooLonLat = [-121.2663397, 37.8350257];
 var sudoroomCoords = fromLonLat(sudoroomLonLat, 'EPSG:3857');
 
 var pointFeature = new Feature({
@@ -77,7 +80,6 @@ var mainLayer = new VectorTileLayer({
   }),
 
   style: customStyle(Style, Fill, Stroke, Icon, Text)
-
 })
 
 var myMap = new Map({
@@ -94,3 +96,89 @@ var myMap = new Map({
 
 });
 
+
+
+myMap.on("singleclick", function(e){
+  if(state !== 'dropping') return;
+
+  var lonLat = toLonLat(e.coordinate);
+  console.log("Dropped pin:", lonLat);
+
+
+  var pointFeature = new Feature({
+    geometry: new Point(e.coordinate),
+    name: 'Null Island',
+    population: 4000,
+    rainfall: 500
+  });
+
+  var pointStyle = new Style({
+    image: new Icon({
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'pixels',
+      src: 'icon.png'
+    })
+  });
+
+  pointFeature.setStyle(pointStyle);
+
+  pointSource.addFeature(pointFeature);
+
+  dropPinBtn.style.backgroundColor = 'gray';
+  state = null;
+});
+
+
+var popupEl = document.getElementById('popup');
+
+var popup = new Overlay({
+  element: popupEl,
+  positioning: 'bottom-center',
+  stopEvent: false,
+  offset: [-30, -100]
+});
+myMap.addOverlay(popup);
+
+// display popup on click
+myMap.on('click', function(e) {
+  var feature = myMap.forEachFeatureAtPixel(
+    e.pixel, 
+    function(feature) {
+      return feature;
+    }
+  );
+
+  var geom;
+  if(feature) {
+    geom = feature.getGeometry();
+  }
+  if(geom && typeof geom.getCoordinates === 'function') {
+    var coordinates = geom.getCoordinates();
+
+    popup.setPosition(coordinates);
+
+    popupEl.style.display = 'block';
+    popupEl.style.zIndex = 1000;
+  } else {
+    popupEl.style.display = 'none';
+  }
+})
+
+var dropPinBtn = document.getElementById('drop-pin');
+var state;
+
+dropPinBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  if(state === 'dropping') {
+    dropPinBtn.style.backgroundColor = 'gray';
+    state = null;
+    return;
+  }
+
+  state = 'dropping';
+  dropPinBtn.style.backgroundColor = 'green';
+  
+});
