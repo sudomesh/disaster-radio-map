86253aebc0a2926f4a334b5b34c425938f39af88
Marc Juul <github@juul.io>
2018-08-14 02:12:19 -0700

    picking pin icons working

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ var myMap = new Map({
 });
 
 
-function dropPin(coord) {
+function dropPin(coord, type) {
   var pointFeature = new Feature({
     geometry: new Point(coord),
     name: 'Null Island',
@@ -107,34 +107,156 @@ function dropPin(coord) {
     rainfall: 500
   });
 
+  var iconFile = (type) ? (type+'.png') : 'icon.png';
+
   var pointStyle = new Style({
     image: new Icon({
       anchor: [0.5, 46],
       anchorXUnits: 'fraction',
       anchorYUnits: 'pixels',
-      src: 'icon.png'
+      src: iconFile
     })
   });
 
   pointFeature.setStyle(pointStyle);
 
   pointSource.addFeature(pointFeature);
+  return pointFeature;
+}
+
+function changePinIcon(feature, src) {
+  var pointStyle = new Style({
+    image: new Icon({
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'pixels',
+      src: src
+    })
+  });
+  feature.setStyle(pointStyle);
+}
+
+var pinDialogEl = document.getElementById('icon-select');
+
+
+var pinDialog = new Overlay({
+  element: pinDialogEl,
+  positioning: 'bottom-center',
+  stopEvent: false,
+  offset: [10, -240]
+});
+myMap.addOverlay(pinDialog);
+
+var curPin;
+
+function startPinCreation(coordinate) {
+  state = null;
+
+  document.getElementById('icon-select-step1').style.display = 'block';
+  document.getElementById('icon-select-step2').style.display = 'none';
+  document.getElementById('icon-select-step3').style.display = 'none';
+
+  pinDialog.setPosition(coordinate);
+  
+  pinDialogEl.style.display = 'block';  
+  pinDialogEl.style.zIndex = 3000;
+  var pin = dropPin(coordinate);
+
+  var lonLat = toLonLat(coordinate);
+  console.log("Dropped pin:", lonLat);
+
+  curPin = {
+    coordinate: coordinate,
+    feature: pin
+  };
+
+}
+
+
+document.getElementById('resource-btn').addEventListener('click', function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  if(!curPin) return;
+
+  document.getElementById('resource-or-need').innerHTML = "the resource";
+
+  curPin.type = 'resource';
+
+  pinIconSelect()
+});
+
+document.getElementById('need-btn').addEventListener('click', function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  if(!curPin) return;
+
+  document.getElementById('resource-or-need').innerHTML = "needed";
+
+  curPin.type = 'need';
+
+  pinIconSelect();
+});
+
+
+var icons = document.getElementById('icon-select-step2').querySelectorAll("img")
+var i;
+for(i=0; i < icons.length; i++) {
+  icons[i].addEventListener('click', pinIconSelected);
+}
+
+function pinIconSelect() {
+
+  document.getElementById('icon-select-step1').style.display = 'none';
+  document.getElementById('icon-select-step2').style.display = 'block';
+  document.getElementById('icon-select-step3').style.display = 'none';
+
+
+}
+
+function pinIconSelected(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  document.getElementById('icon-select-step1').style.display = 'none';
+  document.getElementById('icon-select-step2').style.display = 'none';
+  document.getElementById('icon-select-step3').style.display = 'block';
+  document.getElementById('pin-description').value = '';
+
+  var filename = e.target.src.split('/').pop();
+  var iconName = filename.replace(/\.[^\.]+$/, '');
+  curPin.type = iconName;
+
+
+  changePinIcon(curPin.feature, filename);
+
+  console.log("base", iconName)
+}
+
+document.getElementById('save-pin-btn').addEventListener('click', function(e) {
+  var desc = document.getElementById('save-pin-btn').innerHTML;
+  console.log("Desc:", desc);
+
+
+  var lonLat = toLonLat(curPin.coordinate);
+  finalizePin(lonLat, curPin.type, desc);
+});
+
+function finalizePin(lonLat, type, desc) {
+  socketSendPin(lonLat, type, desc, function(err) {
+    if(err) return console.error(err);
+
+    curPin = null;
+    dropPinBtn.style.backgroundColor = 'gray';
+    pinDialogEl.style.display = 'none';
+    state = null;
+
+  });
 }
 
 myMap.on("singleclick", function(e) {
   if(state !== 'dropping') return;
 
-  var lonLat = toLonLat(e.coordinate);
-  console.log("Dropped pin:", lonLat);
-
-  dropPin(e.coordinate);
-
-  socketSendPin(lonLat, function(err) {
-    if(err) return alert("Failed to send pin"); // TODO bad
-
-    dropPinBtn.style.backgroundColor = 'gray';
-    state = null;
-  });
+  startPinCreation(e.coordinate);
 });
 
 
@@ -207,7 +329,7 @@ socket.connect(function(err, isConnected) {
 });
 
 // send pin over the websocket
-function socketSendPin(pos, cb) {
+function socketSendPin(pos, type, desc, cb) {
   // TODO we should let the user know they're not connected
   if(!socketConnected) {
     console.error("socket not connected so not sending pin position");
@@ -215,7 +337,11 @@ function socketSendPin(pos, cb) {
     return;
   }
 
-  var msg = JSON.stringify(pos);
+  var msg = JSON.stringify({
+    pos: pos,
+    type: type,
+    desc: desc
+  });
 
   socket.send('m', msg, function(err) {
     if(err) {
@@ -227,10 +353,10 @@ function socketSendPin(pos, cb) {
   });
 
   socket.addListener('m', function(namespace, data) {
-    var lonLat = JSON.parse(data)
-
+    var o = JSON.parse(data);
+    var lonLat = o.pos;
     var coord = fromLonLat(lonLat);
 
-    dropPin(coord);
+    dropPin(coord, type);
   })
 }
